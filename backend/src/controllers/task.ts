b1441006a6c12a6356ed63d44bf94cfe9933f95c
Sofia Heim <soheim@ucsd.edu>
2026-06-01 17:09:24 -0700

    onboarding 1.2

--- backend/src/controllers/task.ts
+++ backend/src/controllers/task.ts
@@ -90,3 +90,38 @@ export const removeTask: RequestHandler = async (req, res, next) => {
     next(error);
   }
 };
+
+type UpdateTaskBody = {
+  _id: string;
+  title: string;
+  description?: string;
+  isChecked: boolean;
+};
+
+export const updateTask: RequestHandler = async (req, res, next) => {
+  const { id } = req.params;
+  const errors = validationResult(req);
+  const { _id, title, description, isChecked } = req.body as UpdateTaskBody;
+
+  try {
+    validationErrorParser(errors);
+
+    if (id !== _id) {
+      res.status(400);
+      return;
+    }
+    const originalTask = await TaskModel.findByIdAndUpdate(id, {
+      title,
+      description,
+      isChecked,
+    });
+    if (originalTask === null) {
+      throw createHttpError(404, "Task not found.");
+    }
+    const updatedTask = await TaskModel.findById(id);
+
+    res.status(200).json(updatedTask);
+  } catch (error) {
+    next(error);
+  }
+};
--- frontend/src/components/TaskItem.tsx
+++ frontend/src/components/TaskItem.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import { Dialog } from "@tritonse/tse-constellation";
+import React, { useState } from "react";
+import { updateTask } from "src/api/tasks";
 import { CheckButton } from "src/components";
 import styles from "src/components/TaskItem.module.css";
 
@@ -8,14 +10,48 @@ export type TaskItemProps = {
   task: Task;
 };
 
-export function TaskItem({ task }: TaskItemProps) {
+export function TaskItem({ task: initialTask }: TaskItemProps) {
+  const [task, setTask] = useState<Task>(initialTask);
+  const [isLoading, setLoading] = useState<boolean>(false);
+
+  const [errorModalMessage, setErrorModalMessage] = useState<string | null>(null);
+
+  const handleToggleCheck = async () => {
+    setLoading(true);
+
+    try {
+      const result = await updateTask({ ...task, isChecked: !task.isChecked });
+      if (result.success) {
+        setTask(result.data);
+      } else {
+        setErrorModalMessage(result.error);
+      }
+    } catch (error) {
+      setErrorModalMessage(error instanceof Error ? error.message : String(error));
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <div className={styles.item}>
-      <CheckButton checked={task.isChecked} />
+      <CheckButton
+        checked={task.isChecked}
+        onPress={() => {
+          void handleToggleCheck();
+        }}
+        disabled={isLoading}
+      />
       <div className={`${styles.textContainer} ${task.isChecked ? styles.checked : ""}`}>
         <span className={styles.title}>{task.title}</span>
         {task.description && <span>{task.description}</span>}
       </div>
+      {errorModalMessage && (
+        <Dialog
+          isOpen={!!errorModalMessage}
+          onClose={() => setErrorModalMessage(null)}
+          message={errorModalMessage}
+        />
+      )}
     </div>
   );
 }
